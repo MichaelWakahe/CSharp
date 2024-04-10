d86efe6778dc37024f153d57ce8ab52878bc3012
Michael Wakahe <mikewakahe@gmail.com>
2024-04-10 15:27:44 +0300

    Ch 1 improvements

--- Books/C Sharp 10 and NET 6/Chapter01/HelloCS/Program.cs
+++ Books/C Sharp 10 and NET 6/Chapter01/HelloCS/Program.cs
@@ -10,3 +10,13 @@ namespace HelloCS
         }
     }
 }
+
+/*
+In C# 9 or later you can eliminate boilerplate code above. This is known as top-level programs.
+
+The following is the new top-level program minimum console app:
+
+    using System;
+    Console.WriteLine("Hello World!");
+
+*/
